999aaaff7b33e37c4db2d1f2ee5c064773630bd6
ngocthieuff <ptnngoc22@gmail.com>
2022-10-10 16:27:19 +0700

    setup react redux

---
+++ react-redux-demoow/src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+  return (
+    <>
+      <h4>Number of cakes:</h4>
+      <button>Buy cake</button>
+    </>
+  )
+}
+
+export default CakeContainer
